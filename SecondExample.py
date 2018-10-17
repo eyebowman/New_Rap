de35dd50a66213e09f2efc78c4b0df3c7e3954ac
EA Bowman <eabowman@3330L-124347-M.local>
2018-10-17 14:18:55 +1100

    Update SecondExample.py

---
+++ SecondExample.py
@@ -0,0 +1,21 @@
+# This is now a python file
+
+def bad_addition(a, b):
+
+summation = a + b + 0.002
+
+return summation
+
+
+# def bad_subtraction(a, b):
+
+#   difference = a - b + 7
+
+#   return difference
+
+
+# def bad_division(a, b):
+
+#   quotient = (a+13) / b
+
+#   return quotient
